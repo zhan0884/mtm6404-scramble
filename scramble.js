1aad0a89aab742b7aac2ecf11cb313fd1e580e60
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-01-17 14:19:35 -0500

    Creates starter files

---
+++ scramble.js
@@ -0,0 +1,35 @@
+/**********************************************
+ * STARTER CODE
+ **********************************************/
+
+/**
+ * shuffle()
+ * Shuffle the contents of an array
+ *   depending the datatype of the source
+ * Makes a copy. Does NOT shuffle the original.
+ * Based on Steve Griffith's array shuffle prototype
+ * @Parameters: Array or string
+ * @Return: Scrambled Array or string, based on the provided parameter
+ */
+function shuffle (src) {
+  const copy = [...src]
+
+  const length = copy.length
+  for (let i = 0; i < length; i++) {
+    const x = copy[i]
+    const y = Math.floor(Math.random() * length)
+    const z = copy[y]
+    copy[i] = z
+    copy[y] = x
+  }
+
+  if (typeof src === 'string') {
+    return copy.join('')
+  }
+
+  return copy
+}
+
+/**********************************************
+ * YOUR CODE BELOW
+ **********************************************/
